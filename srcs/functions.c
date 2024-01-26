8887b30d55dfc97cabb938a3707dc6a3bad81200
kfujita <kfujita@student.42tokyo.jp>
2024-01-27 02:00:55 +0900

    row/colがboardかshapeかわかりやすくした

--- srcs/functions.c
+++ srcs/functions.c
@@ -2,14 +2,15 @@
 #include <tetris.h>
 
 bool can_put_tetromino(const Tetromino *shape) {
-	for (int row = 0; row < shape->width; row++) {
-		for (int col = 0; col < shape->width; col++) {
-			if ((shape->col + col < 0 || shape->col + col >= COL_COUNT || shape->row + row >= ROW_COUNT)) {
-				if (shape->array[row][col]) {
+	for (int shape_row = 0; shape_row < shape->width; shape_row++) {
+		for (int shape_col = 0; shape_col < shape->width; shape_col++) {
+			int board_col = shape->col + shape_col;
+			int board_row = shape->row + shape_row;
+			if ((board_col < 0 || COL_COUNT <= board_col || ROW_COUNT <= board_row)) {
+				if (shape->array[shape_row][shape_col]) {
 					return false;
 				}
-
-			} else if (Table[shape->row + row][shape->col + col] && shape->array[row][col]) {
+			} else if (Table[board_row][board_col] && shape->array[shape_row][shape_col]) {
 				return false;
 			}
 		}
@@ -35,10 +36,12 @@ void tetromino_rotate(Tetromino *shape) {
 
 void print_current_table() {
 	char Buffer[ROW_COUNT][COL_COUNT] = {0};
-	for (int row = 0; row < current.width; row++) {
-		for (int col = 0; col < current.width; col++) {
-			if (current.array[row][col]) {
-				Buffer[current.row + row][current.col + col] = current.array[row][col];
+	for (int shape_row = 0; shape_row < current.width; shape_row++) {
+		for (int shape_col = 0; shape_col < current.width; shape_col++) {
+			if (current.array[shape_row][shape_col]) {
+				int board_row = current.row + shape_row;
+				int board_col = current.col + shape_col;
+				Buffer[board_row][board_col] = current.array[shape_row][shape_col];
 			}
 		}
 	}
@@ -47,9 +50,9 @@ void print_current_table() {
 		printw(" ");
 	}
 	printw(GAME_TITLE "\n");
-	for (int row = 0; row < ROW_COUNT; row++) {
-		for (int col = 0; col < COL_COUNT; col++) {
-			printw("%c ", (Table[row][col] + Buffer[row][col]) ? CHAR_BLOCK : CHAR_EMPTY);
+	for (int board_row = 0; board_row < ROW_COUNT; board_row++) {
+		for (int board_col = 0; board_col < COL_COUNT; board_col++) {
+			printw("%c ", (Table[board_row][board_col] + Buffer[board_row][board_col]) ? CHAR_BLOCK : CHAR_EMPTY);
 		}
 		printw("\n");
 	}
