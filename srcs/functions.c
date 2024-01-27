2ccba8bc555d099197a98dff33d9d9382849812e
kfujita <kfujita@student.42tokyo.jp>
2024-01-27 18:48:40 +0900

    関数分割など

--- srcs/functions.c
+++ srcs/functions.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 #include <tetris.h>
 
 bool can_put_tetromino(const Tetromino *shape) {
@@ -24,37 +25,79 @@ void tetromino_rotate(Tetromino *shape) {
 	for (
 			int row_old = 0, col_new = 0;
 			row_old < width;
-			col_new = ++row_old) {
+			col_new = ++row_old
+	) {
 		for (
 				int col_old = 0, row_new = width - 1;
 				col_old < width;
-				row_new = width - ++col_old - 1) {
+				row_new = width - ++col_old - 1
+		) {
 			shape->array[row_old][col_old] = temp.array[row_new][col_new];
 		}
 	}
 }
 
-void print_current_table() {
-	char Buffer[ROW_COUNT][COL_COUNT] = {0};
-	for (int shape_row = 0; shape_row < current_shape.width; shape_row++) {
-		for (int shape_col = 0; shape_col < current_shape.width; shape_col++) {
-			if (current_shape.array[shape_row][shape_col]) {
-				int board_row = current_shape.row + shape_row;
-				int board_col = current_shape.col + shape_col;
-				Buffer[board_row][board_col] = current_shape.array[shape_row][shape_col];
+#define _SIZEOF_CHAR_CELL (2)
+static void _set_shape_to_buf(
+		char (*buf)[ROW_COUNT][COL_COUNT * _SIZEOF_CHAR_CELL],
+		const char *shape,
+		int row,
+		int col,
+		int height,
+		int width
+) {
+	for (int shape_cursor_row = 0; shape_cursor_row < height; shape_cursor_row++) {
+		for (int shape_cursor_col = 0; shape_cursor_col < width; shape_cursor_col++) {
+			int shape_index = (shape_cursor_row * width) + shape_cursor_col;
+			int buf_row = row + shape_cursor_row;
+			int buf_col = (col + shape_cursor_col) * _SIZEOF_CHAR_CELL;
+			if (shape[shape_index]) {
+				(*buf)[buf_row][buf_col] = CHAR_BLOCK;
+			} else if ((*buf)[buf_row][buf_col] != CHAR_BLOCK) {
+				(*buf)[buf_row][buf_col] = CHAR_EMPTY;
 			}
 		}
 	}
+}
+
+static void _set_current_table_chars(
+		char (*buf)[ROW_COUNT][COL_COUNT * _SIZEOF_CHAR_CELL]
+) {
+	memset(buf, ' ', ROW_COUNT * COL_COUNT * _SIZEOF_CHAR_CELL);
+
+	_set_shape_to_buf(
+			buf,
+			&(current_shape.array[0][0]),
+			current_shape.row,
+			current_shape.col,
+			current_shape.width,
+			BLOCK_COUNT
+	);
+	_set_shape_to_buf(
+			buf,
+			&(Table[0][0]),
+			0,
+			0,
+			ROW_COUNT,
+			COL_COUNT
+	);
+
+	for (int row = 0; row < (ROW_COUNT - 1); row++) {
+		(*buf)[row][(COL_COUNT * _SIZEOF_CHAR_CELL) - 1] = '\n';
+	}
+	(*buf)[ROW_COUNT - 1][(COL_COUNT * _SIZEOF_CHAR_CELL) - 1] = '\0';
+}
+
+void print_current_table() {
+	static char Buffer[ROW_COUNT][COL_COUNT * _SIZEOF_CHAR_CELL] = {0};
+
+	_set_current_table_chars(&Buffer);
+
 	clear();
 	for (int col = 0; col < COL_COUNT - ((int)sizeof(GAME_TITLE) - 1); col++) {
 		printw(" ");
 	}
 	printw(GAME_TITLE "\n");
-	for (int board_row = 0; board_row < ROW_COUNT; board_row++) {
-		for (int board_col = 0; board_col < COL_COUNT; board_col++) {
-			printw("%c ", (Table[board_row][board_col] + Buffer[board_row][board_col]) ? CHAR_BLOCK : CHAR_EMPTY);
-		}
-		printw("\n");
-	}
+	printw("%s\n", Buffer);
 	printw("\nScore: %d\n", final_score);
 }
